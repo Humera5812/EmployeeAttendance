f1bb7314657acc1436e10dd7696cbd29283b2efc
Humera5812 <humera198.hshs@gmail.com>
2022-07-15 18:31:38 +0530

    First Commit

---
+++ EmployeeAttendance/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+    namespace Day_3
+{
+    class EmployeeAttendance
+    {
+        public static void Main(string[]args)
+        {
+            Console.WriteLine("Employee is Present");
+            
+        }
+   }
+}
